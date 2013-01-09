1e37bd787dc2132bb62af71120b444e690d13567
Derrick Lockwood <lockwood.derrick@gmail.com>
2013-01-08 19:50:18 -0600

    Added Stuff

---
+++ src/edu/wpi/first/wpilibj/templates/Controller.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+
+/**
+ *
+ * @author Dlock
+ */
+public class Controller {
+    
+    
+    
+}
