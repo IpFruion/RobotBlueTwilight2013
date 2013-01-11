48352a56b669aea6f299eda6c3206b912c51a412
Derrick Lockwood <lockwood.derrick@gmail.com>
2013-01-10 21:15:12 -0600

    Added to Drive Train

---
+++ RobotBlueTwilight2013Drive/src/edu/wpi/first/wpilibj/templates/Constants.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.templates;
+
+/**
+ *
+ * @author Tim & Derrick
+ */
+public interface Constants
+{
+    //Drive Jags
+    public static final int LEFT_JAG_PORT = 1;
+    public static final int RIGHT_JAG_PORT = 2;
+
+    //Joysticks
+    public static final int LEFT_STICK_PORT = 1;
+    public static final int RIGHT_STICK_PORT = 2;
+
+}
