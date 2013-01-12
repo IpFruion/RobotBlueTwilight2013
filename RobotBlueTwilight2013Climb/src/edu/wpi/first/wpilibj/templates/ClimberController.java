f212f4bfb965f11c624fe6b35cb29103a2fcdea2
Luke Davies <supersmilers@me.com>
2013-01-12 16:40:07 -0600

    base setup included

---
+++ RobotBlueTwilight2013Climb/src/edu/wpi/first/wpilibj/templates/ClimberController.java
@@ -0,0 +1,14 @@
+
+package edu.wpi.first.wpilibj.templates;
+import edu.wpi.first.wpilibj.Joystick;
+/**
+ *
+ * @author Robotics
+ */
+public interface ClimberController {
+
+    
+    
+    
+    
+}
--- RobotBlueTwilight2013Climb/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ RobotBlueTwilight2013Climb/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -1,14 +1,26 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+/**----------------------------------------------------------------------------
+ * Copyright (c) FIRST 2013. All Rights Reserved.                             
+ * Open Source Software - may be modified and shared by FRC teams. The code   
+ * must be accompanied by the FIRST BSD license file in the root directory of 
+ * the project.                                                               
+ * ----------------------------------------------------------------------------
+ */
 
 package edu.wpi.first.wpilibj.templates;
 
 
 import edu.wpi.first.wpilibj.SimpleRobot;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Jaguar;
+
+
+
+/**
+ * 
+ * @author luke
+ */
+
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -18,6 +30,10 @@ import edu.wpi.first.wpilibj.SimpleRobot;
  * directory.
  */
 public class RobotTemplate extends SimpleRobot {
+    
+
+
+   
     /**
      * This function is called once each time the robot enters autonomous mode.
      */
