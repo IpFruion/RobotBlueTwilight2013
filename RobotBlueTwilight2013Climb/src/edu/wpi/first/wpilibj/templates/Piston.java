6e3a9bd8831eec245bee1fdbc13dc9125181e3f2
Luke Davies <supersmilers@me.com>
2013-01-17 19:58:09 -0600

    change

--- RobotBlueTwilight2013Climb/src/edu/wpi/first/wpilibj/templates/Piston.java
+++ RobotBlueTwilight2013Climb/src/edu/wpi/first/wpilibj/templates/Piston.java
@@ -4,10 +4,25 @@
  */
 package edu.wpi.first.wpilibj.templates;
 
+import edu.wpi.first.wpilibj.Solenoid;
+
 /**
  *
- * @author Robotics
+ * @author alec
  */
-public interface Piston {
-    
+public class Piston {
+    private Solenoid extend, retract;
+    public Piston(int extendPort, int retractPort){
+        extend = new Solenoid(extendPort);
+        retract = new Solenoid(retractPort);
+    }
+    public void set(boolean state)
+    {
+        extend.set(state);
+        retract.set(!state);
+    }
+    public boolean get()
+    {
+        return extend.get();
+    }
 }
