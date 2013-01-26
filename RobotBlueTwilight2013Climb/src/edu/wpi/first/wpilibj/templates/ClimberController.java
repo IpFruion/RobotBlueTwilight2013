e2265fe562e2e337ff34d66eed02963b10d8e6ef
Luke Davies <supersmilers@me.com>
2013-01-26 15:50:16 -0600

    added source reference

--- RobotBlueTwilight2013Climb/src/edu/wpi/first/wpilibj/templates/ClimberController.java
+++ RobotBlueTwilight2013Climb/src/edu/wpi/first/wpilibj/templates/ClimberController.java
@@ -5,7 +5,7 @@
 package edu.wpi.first.wpilibj.templates;
 import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.DigitalInput;
-
+import edu.wpi.first.wpilibj.DigitalSource;
 
 /*@author Robotics */
 public interface ClimberController {
