272ca40ad2ba454b8752e8b02b8438f5719a2e04
Luke Davies <lchattyshack@live.com>
2013-02-05 19:37:20 -0600

    organized references

--- Robot2013Final/src/edu/wpi/first/wpilibj/templates/BTAutonomous.java
+++ Robot2013Final/src/edu/wpi/first/wpilibj/templates/BTAutonomous.java
@@ -3,10 +3,9 @@
  * and open the template in the editor.
  */
 package edu.wpi.first.wpilibj.templates;
-import edu.wpi.first.wpilibj.Accelerometer;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.Encoder;
 /**
  *
  * @author Luke
