5bef3415e492ca9213addc172fb0bd11830dda74
Luke Davies <lchattyshack@live.com>
2013-02-05 19:43:07 -0600

    added codes

--- Robot2013Final/src/edu/wpi/first/wpilibj/templates/BTAutonomous.java
+++ Robot2013Final/src/edu/wpi/first/wpilibj/templates/BTAutonomous.java
@@ -5,13 +5,14 @@
 package edu.wpi.first.wpilibj.templates;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Gyro;
+import edu.wpi.first.wpilibj.SimpleRobot;
 import edu.wpi.first.wpilibj.Timer;
 /**
  *
  * @author Luke
  */
-public class BTAutonomous {
-  public BTController stick;
+public class BTAutonomous extends SimpleRobot {
+    public BTController stick;
     public DriveTrain drive;
     public CompressorInit comp;
     public Timer timer;
@@ -19,12 +20,21 @@ public class BTAutonomous {
     public Encoder encoder;
     int DRIVE_ENCODER_CHANNEL_A = 1;
     int DRIVE_ENCODER_CHANNEL_B = 2;
-    public void robotInit()
+  public void robotInit()
     {
         stick = new BTController();
         drive = new DriveTrain();
         comp = new CompressorInit();
         timer = new Timer();
         encoder = new Encoder(DRIVE_ENCODER_CHANNEL_A, DRIVE_ENCODER_CHANNEL_B);
-    }  
+    }
+  public void autonomous() {
+      comp.run();
+      timer.start();
+      while (isAutonomous()){
+      encoder.get();
+      }
+      timer.stop();
+      comp.stop();
+    }
 }
