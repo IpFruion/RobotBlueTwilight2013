c315dd446a969a734716eb6dc961c213bb1cbd07
Neo <tmthyndrsn@gmail.com>
2013-02-09 15:06:29 -0600

    Changed some vision code

---
+++ src/edu/wpi/first/wpilibj/templates/ControlBoard.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+/**
+ *
+ * @author Dlock
+ */
+public class ControlBoard extends BTController {
+    
+    public ControlBoard()
+    {
+        
+    }
+    public double getShootMotorSpeed()
+    {
+        //add slider for adjust speed
+        return .9;
+    }
+}
